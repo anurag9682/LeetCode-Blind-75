fd9f09b9c337f55df6c51ec108998ed0d5c21c84
Anurag Dubey <147299537+anurag9682@users.noreply.github.com>
2025-07-19 20:13:19 +0530

    Time: 1 ms (80.07%) | Memory: 15 MB (21.08%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        //.............>>Brute Force Approach<<........
+        // int n = nums.size();
+        // for(int i = 0; i < n; i++) {
+        //     for(int j = i + 1; j < n; j++) {
+        //         if(nums[i] + nums[j] == target) {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+        // // Return an empty vector if no such pair found (handle edge case as needed)
+        // return {};
+
+
+        //........>> Using Hash Map  <<...............
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int complement = target-nums[i];
+            if(mp.find(complement)!=mp.end()){
+                return {mp[complement],i};
             }
+            mp[nums[i]] =i;
         }
-        // Return an empty vector if no such pair found (handle edge case as needed)
-        return {};
-        
+        return {}; 
     }
 };
